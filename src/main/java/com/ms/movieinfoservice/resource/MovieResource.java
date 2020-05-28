3ee3fbc815d5320590236f42fc7f725a6e695567
Sunny Pukkalli <pukkallisunny@gmail.com>
2020-05-28 18:19:01 +0530

    Reverting to Path Variable

--- src/main/java/com/ms/movieinfoservice/resource/MovieResource.java
+++ src/main/java/com/ms/movieinfoservice/resource/MovieResource.java
@@ -5,7 +5,7 @@ import com.ms.movieinfoservice.model.Movie;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -18,8 +18,8 @@ public class MovieResource {
     @Autowired
     private RestTemplate restTemplate;
 
-    @GetMapping
-    public Movie getMovieInfo(@RequestBody Integer movieId){
+    @GetMapping("{movieId}")
+    public Movie getMovieInfo(@PathVariable("movieId") Integer movieId){
 
         // return Random Movie Details
         String movieName = RandomStringUtils.randomAlphabetic(5);
