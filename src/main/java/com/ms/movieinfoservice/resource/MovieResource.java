acc088227ef1995281138c68caf13878201ce5cb
Sunny Pukkalli <pukkallisunny@gmail.com>
2020-05-28 17:44:23 +0530

    Chaning port for local env

--- src/main/java/com/ms/movieinfoservice/resource/MovieResource.java
+++ src/main/java/com/ms/movieinfoservice/resource/MovieResource.java
@@ -5,7 +5,6 @@ import com.ms.movieinfoservice.model.Movie;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
